c884b103113fd71adcd0d491d0755288cde06fb7
Majho2141 <mariaj.munozp@autonoma.edu.co>
2024-05-19 23:10:55 -0500

    Se arreglo un bug en la table productxpart

--- src/models/productXpart.py
+++ src/models/productXpart.py
@@ -5,8 +5,8 @@ from src.config.database import Base
 class Productxpart(Base):
     __tablename__ = 'productxpart'
     id = Column(Integer, primary_key=True, autoincrement=True)
-    id_supplier = Column(String(length=100),ForeignKey('supplier.id'), nullable=False, unique=True)
-    id_branch = Column(String(length=100),ForeignKey('branch.id'), nullable=True )
+    id_supplier = Column(Integer,ForeignKey('supplier.id'), nullable=False, unique=True)
+    id_branch = Column(Integer,ForeignKey('branch.id'), nullable=True )
     id_product = Column(Integer, ForeignKey('product.id'), nullable=False)
     Rbranch = relationship("Branch", back_populates="Rproductxpart")
     Rsupplier = relationship("Supplier", back_populates="Rproductxpart")
